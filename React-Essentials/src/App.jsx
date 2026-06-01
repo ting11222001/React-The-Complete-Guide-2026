816ceb5b3b3afcebeb04e4c298740386d35025d1
Li-Ting Liao <39208708+ting11222001@users.noreply.github.com>
2026-06-01 10:13:21 +0930

    Output dynamic values in a React component

--- React-Essentials/src/App.jsx
+++ React-Essentials/src/App.jsx
@@ -1,10 +1,17 @@
+const headerDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomIndex(inputArrayLength) {
+  return Math.floor(Math.random() * inputArrayLength); // random number between 0 and the length of the input array (exclusive)
+}
+
 function Header() {
+  const randomDescription = headerDescription[getRandomIndex(headerDescription.length)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {randomDescription} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
